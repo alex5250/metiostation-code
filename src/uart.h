17196eae6d1351de9a4719b6a394fd29dbfcdbb0
Alex Zaslavskis <sahsariga111@gmail.com>
2023-09-07 20:00:28 +0300

    fixed formating using clang-format

--- src/uart.h
+++ src/uart.h
@@ -53,7 +53,6 @@
 
 #define BAUD_PRESCALAR (((F_CPU / (USART_BAUD_RATE * 16UL))) - 1)
 
-
 // NOTE: UBBR is a 12 bit register using 8 bits low and 4 bits high
 
 // file scope variable that acts as a 'private data member'
@@ -61,33 +60,35 @@ static char* message;
 // can only be changed via 'public method' usart_set_message()
 
 // Sets the private member data message
-void usart_send_message(char* msg) {
-  message = msg;
+void usart_send_message(char* msg)
+{
+    message = msg;
     // clear transmit buffer
-  UDR0 = 0x00;
-  // enable transmit mode
-  UCSR0B = (1<<TXEN0);
-  for (int idx = 0; message[idx] != 0; idx++) {
-    while ((UCSR0A & (1<<UDRE))== 0);
-    UDR0 = (char) message[idx];
-  }
-  while ((UCSR0A & (1<<UDRE))== 0);
-  UDR0 = '\r';
-  while ((UCSR0A & (1<<UDRE))== 0);
-  UDR0 = '\n';
-  // disable transmit mode
-  UCSR0B = (0 << TXEN0);
+    UDR0 = 0x00;
+    // enable transmit mode
+    UCSR0B = (1 << TXEN0);
+    for (int idx = 0; message[idx] != 0; idx++) {
+        while ((UCSR0A & (1 << UDRE)) == 0)
+            ;
+        UDR0 = (char)message[idx];
+    }
+    while ((UCSR0A & (1 << UDRE)) == 0)
+        ;
+    UDR0 = '\r';
+    while ((UCSR0A & (1 << UDRE)) == 0)
+        ;
+    UDR0 = '\n';
+    // disable transmit mode
+    UCSR0B = (0 << TXEN0);
 }
 
 // initializes the UART for use
-void usart_init() {
-  // reset USDR0 to empty, emptying RX/TX buffer
-  // set baud rate
-  UBRR0H = BAUD_PRESCALAR >> 8;
-  UBRR0L = BAUD_PRESCALAR;
-  // set frame format
-  UCSR0C = (0b11<<1); // set to 8 bit format
+void usart_init()
+{
+    // reset USDR0 to empty, emptying RX/TX buffer
+    // set baud rate
+    UBRR0H = BAUD_PRESCALAR >> 8;
+    UBRR0L = BAUD_PRESCALAR;
+    // set frame format
+    UCSR0C = (0b11 << 1); // set to 8 bit format
 }
-
-
-
